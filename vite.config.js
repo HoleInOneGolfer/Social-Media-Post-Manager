e1adb066265abc7b6d0ea577db22b758c10bd5da
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2024-08-20 23:59:45 -0500

    config update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 
+import legacy from "@vitejs/plugin-legacy";
+
 export default defineConfig({
   base: "/Social-Media-Post-Manager/",
   server: {},
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
 });

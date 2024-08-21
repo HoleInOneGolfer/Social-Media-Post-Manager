c09c93e49386412a0e3730ee3b2a78e957eb65cc
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2024-08-20 23:36:41 -0500

    tested out importing

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import './style.css'
 const { BASE_URL } = import.meta.env;
 
+import alt from './alt';
+
 document.querySelector<HTMLDivElement>( '#app' )!.innerHTML = `Base URL: ${ BASE_URL }`;
+alt();

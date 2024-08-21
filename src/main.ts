faa716d8e54203cae113328d05d91bcf69644df5
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2024-08-21 00:19:33 -0500

    test setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import './style.css'
 const { BASE_URL } = import.meta.env;
 
-import alt from './alt';
-
 document.querySelector<HTMLDivElement>( '#app' )!.innerHTML = `Base URL: ${ BASE_URL }`;
-alt();
---
+++ test/temp.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+
+describe( "temp", () =>
+{
+    it( "temp", () =>
+    {
+        expect( 1 ).toBe( 1 );
+    } );
+} );

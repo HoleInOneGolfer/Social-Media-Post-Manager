f24e097e7c02538072f3cabeef3ea3ab03fb7b6e
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2024-08-21 11:45:33 -0500

    reset to design interface in different program

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import './style.css'
-const { BASE_URL } = import.meta.env;
-
-var app = document.querySelector<HTMLDivElement>( '#app' )!;
---
+++ src/scripts/main.ts
@@ -0,0 +1,10 @@
+import '../styles/style.css';
+const { BASE_URL } = import.meta.env;
+
+import * as util from './utils';
+
+
+document.addEventListener( 'DOMContentLoaded', () =>
+{
+    const main = document.querySelector( 'main' );
+} );

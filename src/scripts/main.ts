538de011583d1e7b7b46d7476244d0d500e1b741
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2024-08-21 18:40:11 -0500

    fixed everything from bad merge

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,28 +1,20 @@
-import '../styles/style.css'
+import '../styles/style.css';
 
-import { createYear } from '../components/calendar.element'
-import { Calendar } from './calendar'
+import { createYear } from '../components/calendar.element';
+import { Calendar } from './calendar';
 
-const { BASE_URL } = import.meta.env
+const { BASE_URL } = ( import.meta as any ).env;
+
+console.log( 'Base URL:', BASE_URL )
 
 document.addEventListener( 'DOMContentLoaded', () =>
 {
-  console.log( 'Base URL:', BASE_URL )
-  const main = document.querySelector( 'main' )
-document.addEventListener( 'DOMContentLoaded', () =>
-{
-  console.log( 'Base URL:', BASE_URL )
   const main = document.querySelector( 'main' )
 
   const calendar: Calendar = new Calendar( 2024 )
-  const calendar: Calendar = new Calendar( 2024 )
 
-  if ( main )
-  {
-    main.appendChild( createYear( calendar.days ) )
   if ( main )
   {
     main.appendChild( createYear( calendar.days ) )
   }
-} )
 } )

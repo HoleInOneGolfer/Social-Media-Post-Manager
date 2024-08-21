3a2c2f16aff5520511a8e324445a6abaaf135aa3
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2024-08-21 16:10:28 -0500

    idk what i chagned

--- src/scripts/calendar.ts
+++ src/scripts/calendar.ts
@@ -1,35 +1,30 @@
-export function getDates ( year: number ): Date[]
-{
-    let dates: Date[] = [];
-    for (
-        let current = new Date( year, 0, 1 );
-        current.getFullYear() === year;
-        current.setDate( current.getDate() + 1 )
-    )
-    {
-        dates.push( new Date( current ) );
-    }
+export function getDates(year: number): Date[] {
+  const dates: Date[] = []
+  for (
+    let current = new Date(year, 0, 1);
+    current.getFullYear() === year;
+    current.setDate(current.getDate() + 1)
+  ) {
+    dates.push(new Date(current))
+  }
 
-    return dates;
+  return dates
 }
 
-export interface Day
-{
-    date: Date;
-    events: string[];
+export interface Day {
+  date: Date
+  events: string[]
 }
 
-export function createCalendar ( year: number ): Day[][]
-{
-    const dates = getDates( year );
-    const calendar: Day[][] = Array.from( { length: 12 }, () => [] );
+export function createCalendar(year: number): Day[][] {
+  const dates = getDates(year)
+  const calendar: Day[][] = Array.from({ length: 12 }, () => [])
 
-    for ( let m = 0; m < 12; m++ )
-    {
-        calendar[ m ] = dates
-            .filter( ( date ) => date.getMonth() === m )
-            .map( ( date ) => ( { date, events: [] } ) );
-    }
+  for (let m = 0; m < 12; m++) {
+    calendar[m] = dates
+      .filter((date) => date.getMonth() === m)
+      .map((date) => ({ date, events: [] }))
+  }
 
-    return calendar;
+  return calendar
 }
--- test/temp.test.ts
+++ test/temp.test.ts
@@ -1,9 +1,7 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from 'vitest'
 
-describe( "temp", () =>
-{
-    it( "temp", () =>
-    {
-        expect( 1 ).toBe( 1 );
-    } );
-} );
+describe('temp', () => {
+  it('temp', () => {
+    expect(1).toBe(1)
+  })
+})

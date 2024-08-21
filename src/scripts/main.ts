af59927253e53b983ee77d1e8b1ee2a981464961
Ben Bowles <benjamin.d.bowles@gmail.com>
2024-08-21 18:10:34 -0500

    Dev (#13)
    * editor
    
    * refactor and test
    
    * editor stuff

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,17 +1,19 @@
 import '../styles/style.css'
 
 import { createYear } from '../components/calendar.elements'
-import { createCalendar } from './calendar'
+import { Calendar } from './calendar'
 
 const { BASE_URL } = import.meta.env
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Base URL:', BASE_URL)
-  const main = document.querySelector('main')
+document.addEventListener( 'DOMContentLoaded', () =>
+{
+  console.log( 'Base URL:', BASE_URL )
+  const main = document.querySelector( 'main' )
 
-  const calendar = createCalendar(2024)
+  const calendar: Calendar = new Calendar( 2024 )
 
-  if (main) {
-    main.appendChild(createYear(calendar))
+  if ( main )
+  {
+    main.appendChild( createYear( calendar.days ) )
   }
-})
+} )
